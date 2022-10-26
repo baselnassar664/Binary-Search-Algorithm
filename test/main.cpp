53c735f3b3369a4755d76b7a088be2525c5389e5
baselnassar664 <67750426+baselnassar664@users.noreply.github.com>
2022-10-26 13:38:00 +0200

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -11,30 +11,54 @@ using namespace std;
 
 
 int main(){
+
+//liner search
 int n,numberTOFind;
 cin>>n>>numberTOFind;
-vector<int> arr(n);
+int arr[n];
+bool found=false;
 for(int i=0;i<n;i++){
     cin >>arr[i];
 }
-sort(arr.begin(),arr.end());
-int l=0,r=n-1,m;
-bool found=false;
+
+for(int i=0;i<n;i++){
+   if(arr[i]==numberTOFind){
+     found=true;
+    break;
+   }
+
+}
+if(found==true)
+    cout<<"number found"<<endl;
+else
+    cout<<"number not found";
+
+    
+//binary search
+int n1,numberTOFindd;
+cin>>n1>>numberTOFindd;
+vector<int> vec(n);
+for(int i=0;i<n1;i++){
+    cin >>vec[i];
+}
+sort(vec.begin(),vec.end());
+int l=0,r=n1-1,m;
+bool foundd=false;
 while(l<=r){
   m=(l+r)/2;
-  if(arr[m]==numberTOFind){
-    found=true;
+  if(vec[m]==numberTOFindd){
+    foundd=true;
     break;
   }
-  else if(arr[m]>numberTOFind){
+  else if(vec[m]>numberTOFindd){
     r=m-1;
   }
-  else if(arr[m]<numberTOFind){
+  else if(vec[m]<numberTOFindd){
     l=m+1;
   }
 
 }
-if(found==true)
+if(foundd==true)
     cout<<"number found"<<endl;
 else
     cout<<"number not found";
